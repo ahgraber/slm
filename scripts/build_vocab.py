8a37a207b3f1be0056f7cd31964e8efffeedca01
ahgraber <ahgraber@ninerealmlabs.com>
2023-12-20 08:03:42 -0500

    fix: update vocab builder for dataset,preprocessing changes

--- scripts/build_vocab.py
+++ scripts/build_vocab.py
@@ -8,26 +8,15 @@ from pathlib import Path
 import pickle
 import random
 import sys
-from typing import Any, Callable, Iterable, Optional, Union
 
 from tqdm import tqdm
 
 import numpy as np
 
-import datasets
-from tokenizers import Regex, Tokenizer, models, normalizers, pre_tokenizers
-import torch
-
 sys.path.insert(0, str(Path(__file__ + "/../../").resolve()))
-from slm.preprocess import (  # NOQA: E402
-    SentencePreTokenizer,
-    blob_to_sentences,
-    pipeline,
-    sentences_to_words,
-    wiki_remove_headings,
-    wiki_truncate_appedices,
-)
-from slm.utils import flatten, get_project_root, init_nltk, init_spacy  # NOQA: E402
+from slm.datasets import DATASETS, LOADER_KWARGS, MAP_KWARGS, bookcorpus, commoncrawl, wikipedia  # NOQA: E402
+from slm.preprocess import batch_map, parse_words  # NOQA: E402
+from slm.utils import flatten, get_project_root  # NOQA: E402
 from slm.word2vec.vocab import Vocab  # NOQA: E402
 
 # %%
@@ -44,24 +33,10 @@ ARTIFACT_DIR = ROOT_DIR / "artifacts"
 DATA_DIR = ROOT_DIR / "data"
 
 # %%
-SEED = 1337
-random.seed(SEED)
-np.random.seed(SEED)
-rnd = np.random.default_rng(SEED)
-torch.manual_seed(SEED)
-if torch.cuda.is_available():
-    torch.cuda.manual_seed_all(SEED)
-if torch.backends.mps.is_available():
-    torch.mps.manual_seed(SEED)
-
-# %%
-init_nltk(model="punkt", savedir=DATA_DIR / "nltk")
-init_spacy(model="en_core_web_sm")
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-d", "--dataset", required=True, type=str)
+    parser.add_argument("-d", "--dataset", required=True, choices=DATASETS)
     parser.add_argument("-n", "--name", required=False, type=str, default=None)
     parser.add_argument("-s", "--split", required=False, type=str, default="train")
     parser.add_argument("-k", "--key", required=False, type=str, default="text")
@@ -78,78 +53,54 @@ if __name__ == "__main__":
 
     if save_path.exists():
         print("File exists at save path.  Continuing will overwrite existing file.")
-        prompt = str(input("Continue? [y/N]  "))
-        if prompt.lower() not in ["y", "yes"]:
+        response = str(input("Continue? [y/N]  "))
+        if response.lower() not in ["y", "yes"]:
             print("Exiting at user request.")
             SystemExit()
 
-    ds_kwargs = {
-        # "download_mode": "force_redownload",  # if corrupted
-        "download_mode": "reuse_cache_if_exists",
-        # "download_mode": "reuse_dataset_if_exists",
-        "verification_mode": "basic_checks",
-        # "verification_mode": "all_checks",
-    }
+    loader_kwargs = LOADER_KWARGS
+    loader_kwargs["download_mode"] = "reuse_cache_if_exists"
 
-    ds_len = datasets.load_dataset_builder(dataset, name).info.splits[split].num_examples
-    logger.info(f"{dataset}'s '{split}' split has {ds_len} examples")
+    map_kwargs = MAP_KWARGS
+    map_kwargs["input_columns"] = key
 
-    ds = datasets.load_dataset(
-        dataset,
-        name=name,
-        split=split,
-        num_proc=8,
-        **ds_kwargs,
-    )
-    ds = ds.to_iterable_dataset()
-    ds = ds.select_columns(key)
+    match dataset:
+        case "bookcorpus":
+            dset, dsamples = bookcorpus(
+                split=split,
+                key=key,
+                loader_kwargs=loader_kwargs,
+                map_kwargs=map_kwargs,
+                iterable=True,
+            )
+        case "commoncrawl":
+            dset, dsamples = commoncrawl(
+                name=name if name else "realnewslike",
+                split=split,
+                key=key,
+                loader_kwargs=loader_kwargs,
+                map_kwargs=map_kwargs,
+                iterable=True,
+            )
 
-    # normalize control chars
-    prenormalizer = normalizers.Replace(Regex(r"[\p{Other}&&[^\n\t\r]]"), "\n")
-    # use nltk punkt sentence splitter
-    sentence_splitter = pre_tokenizers.PreTokenizer.custom(SentencePreTokenizer())
+        case "wikipedia":
+            dset, dsamples = wikipedia(
+                name=name if name else "20231101.en",
+                split=split,
+                key=key,
+                loader_kwargs=loader_kwargs,
+                map_kwargs=map_kwargs,
+                iterable=True,
+            )
 
-    # NOTE: normalizer and pretokenizer must be same as those in Tokenizer used for modeling
-    normalizer = normalizers.Sequence(
-        [
-            normalizers.NFKD(),
-            normalizers.StripAccents(),
-            normalizers.Lowercase(),
-            normalizers.Replace(Regex(r"[\p{Other}&&[^\n\t\r]]"), ""),  # normalize control chars
-            normalizers.Replace(Regex(r"[\s]"), " "),  # normalize whitespace
-        ]
-    )
-    word_splitter = pre_tokenizers.Whitespace()
+    logger.info(f"{dataset}'s '{split}' split has {dsamples} records")
 
-    preprocess = partial(
-        pipeline,
-        key=key,
-        fn=blob_to_sentences,
-        kwargs={
-            "is_wiki": dataset.__contains__("wikipedia"),
-            "normalizer": prenormalizer,
-            "splitter": sentence_splitter,
-        },
-    )
-    tokenize = partial(
-        pipeline,
-        key=key,
-        fn=sentences_to_words,
-        kwargs={
-            "normalizer": normalizer,
-            "splitter": word_splitter,
-        },
-    )
-
-    # if wiki is iterabledataset, preprocessor executes only as batch is called
-    batch_size = 64
-    ds = ds.map(preprocess, input_columns=key, batched=True, batch_size=batch_size)
-    ds = ds.map(tokenize, input_columns=key, batched=True, batch_size=batch_size)
+    dset = dset.map(partial(batch_map, key=key, fn=parse_words), **map_kwargs)
 
     logger.info("Begin processing dataset & counting...")
-    iterds = iter(ds)
+    iterds = iter(dset)
     counter = Counter()
-    for blob in tqdm(iterds, total=ds_len):
+    for blob in tqdm(iterds, total=dsamples):
         counter.update(flatten(blob[key]))
     else:
         logger.info("Counting complete")
